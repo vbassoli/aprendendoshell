8b82eeb575c06fff798714ecc1dc7f9b453bb1fb
Gustavo <gustavo@logicus.com.br>
2013-10-06 16:08:51 -0300

    Create mostrakernel.sh

---
+++ mostrakernel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# mostrakernel.sh
+
+# limpando a tela
+clear
+
+echo "
+Legenda: Nome do Kernel, Nome da Máquina, Versão do Kernel utilizada, Versão de compilação do Kernel, Arquitetura, Sistema.
+"
+
+uname -a
