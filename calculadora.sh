6e87c76fbe65681649899035f1b65850177e0fc2
Gustavo <gustavo@logicus.com.br>
2013-10-06 01:22:50 -0300

    Update calculadora.sh

--- calculadora.sh
+++ calculadora.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 # calculadora.sh
 # limpando a tela
+# script comum, que serve bem ao aprendizado
+
 clear
 # calculadora propriamente dita
 while true
 do
-	echo -e "Calcularadora"
+	echo -e "Calcularadora" # imprimindo mensagem
 	echo -e "Pressione CTRL+C para encerrar a calculadora\n"
-	read -p "Digite o primeiro número:" numero_1
+	read -p "Digite o primeiro número:" numero_1 # pegando resposta do usuário e criando variável
 	read -p "Digite o operador desejado:" operador
 	read -p "Digite o segundo número:" numero_2
-	echo $(($numero_1 $operador $numero_2))
+	echo $(($numero_1 $operador $numero_2)) # isso é muito interessante
 done
 # fim
