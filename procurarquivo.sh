50ec2b3554988996378ce1f41a0ab3fdf3c2c5a5
Gustavo <gustavo@logicus.com.br>
2013-10-06 02:48:51 -0300

    Update procurarquivo.sh

--- procurarquivo.sh
+++ procurarquivo.sh
@@ -5,7 +5,10 @@
 # limpando a tela
 clear
 
+echo "Após achar o arquivo pressione CTRL+C para agilizar o término do script."
 echo "Digite o nome do arquivo que deseja procurar:"
 read arquivo
 
 sudo find / -name $arquivo
+
+# fim
