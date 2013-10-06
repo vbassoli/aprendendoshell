4c39fd27bda53cb3e993b4ce41ebb935d7857eee
Gustavo <gustavo@logicus.com.br>
2013-10-06 16:09:52 -0300

    Update mostrakernel.sh

--- mostrakernel.sh
+++ mostrakernel.sh
@@ -4,8 +4,6 @@
 # limpando a tela
 clear
 
-echo "
-Legenda: Nome do Kernel, Nome da Máquina, Versão do Kernel utilizada, Versão de compilação do Kernel, Arquitetura, Sistema.
-"
-
+echo "Legenda: Nome do Kernel, Nome da Máquina, Versão do Kernel utilizada, Versão de compilação do Kernel, Arquitetura, Sistema."
+# usando uname-a para expor as informações
 uname -a
