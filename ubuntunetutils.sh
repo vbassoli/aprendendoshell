8474cd0f5752a9c273a7faecd4a62dff08257355
Gustavo <gustavo@logicus.com.br>
2013-10-06 03:39:16 -0300

    Create ubuntunetutils.sh

---
+++ ubuntunetutils.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# ubuntunetutils.sh
+# instala programas interessantes para um administrador de redes
+
+# limpando a tela
+clear
+
+# informando o usuário
+echo "Instalando programas interessantes para administração de redes no Ubuntu..."
+
+# dando um tempo para o usuário ler a mensagem anterior
+sleep 3
+
+# instalando programas
+sudo apt-get install -y wireshark pbnj etherape tcpdump iptraf
+
+# informando o usuário dos programas instalados
+echo "Foram instalados os programas Wireshark, Genlist, Etherape, Tcpdump e Iptraf. Bom trabalho!"
+
+# fim
