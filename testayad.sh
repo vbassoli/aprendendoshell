cf79040331c42760c0afa8c97af459147e21fcbf
Gustavo <gustavo@logicus.com.br>
2013-10-10 17:35:25 -0300

    Create testayad.sh

---
+++ testayad.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# testayad.sh
+# serve para testar pra ver se o Yad está funcionando
+
+yad --title "Testando Yad..." --width 250 --text="Olá mundo cruel" --button "Clique aqui":0
+
+# fim
