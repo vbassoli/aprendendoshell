c17d21a1e7d4c2c16e160f8b11f5bc8baa5d811d
Gustavo <gustavo@logicus.com.br>
2013-10-10 00:18:44 -0300

    Create newslogicus.sh

---
+++ newslogicus.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# newslogicus.sh
+# procura as últimas 7 notícias do site da logicus
+# imprime na tela do usuário
+
+clear
+
+lynx -dump -nolist http://logicus.com.br |
+ grep '^[A-Z]' |
+ sed 's/Menu//' |
+ sed 's/Próxima agenda//' |
+ sed 's/Eventos//' |
+ sed 's/Informe de Cursos//' |
+ head -n7
