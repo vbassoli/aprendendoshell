6788bf5b16263f8a6a8b88c92684b1b91e028202
Gustavo <gustavo@logicus.com.br>
2013-10-30 14:59:33 -0200

    Update buscaip.sh

--- buscaip.sh
+++ buscaip.sh
@@ -3,5 +3,8 @@
 # funciona com ips da familia 192.168.1.0
 # usa nmap, regex e dialog
 
+# lembre-se que deverá ter o nmap instalado
 nmap -sP 192.168.1.1/24 | grep -E '([0-9]{3}.){2}[0-9].[0-9]{2}' | sed 's/.*for /Ip encontrado: /' > /tmp/ip && 
 dialog --title "Buscador de Ip achou:" --msgbox "`cat /tmp/ip`" 0 0
+
+# fim
