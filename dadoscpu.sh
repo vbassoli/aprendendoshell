39cdf82b00c066ef844ff5b86cfa9212a319f55b
Gustavo <gustavo@logicus.com.br>
2013-10-30 15:01:32 -0200

    Update dadoscpu.sh

--- dadoscpu.sh
+++ dadoscpu.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # dadoscpu.sh
 # mostra informações sobre a CPU usando o kdialog
+# necessita do kdialog
 
+# perceba como a estrutura do kdialog não diferencia tanto de outras como
+# dialog, zenity e yad
 kdialog --title "Mostrando dados da CPU" \
 --msgbox "`cat /proc/cpuinfo`"
+
+# fim
