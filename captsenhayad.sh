d666fa520138e053a7a4ec553d8fb8a2e262a5f1
Gustavo <gustavo@logicus.com.br>
2013-10-30 15:00:22 -0200

    Update captsenhayad.sh

--- captsenhayad.sh
+++ captsenhayad.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # captsenhayad.sh
 # script de exemplo de capturação de senha com yad
+# necessita do yad
 
 yad --title "Capturando senha" --form --align right --field "Usuário:" --field Senha::H --image dialog-password
 
