a8ec783c8fdd6bb9f9e9622859e20e0276f118f1
Gustavo <gustavo@logicus.com.br>
2013-10-30 14:43:02 -0200

    Create interrupcoes.sh

---
+++ interrupcoes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# interrupcoes.sh
+# mostra as interrupções usadas
+# usa kdialog
+
+kdialog --title "Interrupções utilizadas por dispositivos neste momento" \
+--msgbox "`cat /proc/interrupts`"
