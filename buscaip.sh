248f52ee7245aa8bfc75316c517ca2bb40907b72
Gustavo <gustavo@logicus.com.br>
2013-10-30 14:18:55 -0200

    Create buscaip.sh

---
+++ buscaip.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# buscaip.sh
+# funciona com ips da familia 192.168.1.0
+# usa nmap, regex e dialog
+
+nmap -sP 192.168.1.1/24 | grep -E '([0-9]{3}.){2}[0-9].[0-9]{2}' | sed 's/.*for /Ip encontrado: /' > /tmp/ip && 
+dialog --title "Buscador de Ip achou:" --msgbox "`cat /tmp/ip`" 0 0
