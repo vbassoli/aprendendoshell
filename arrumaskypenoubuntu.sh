d9dbbcf1d5c4d8861b3dbded49a375ae67ac8f40
Gustavo <gustavo@logicus.com.br>
2013-10-30 23:54:49 -0200

    Create arrumaskypenoubuntu.sh

---
+++ arrumaskypenoubuntu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# arrumaskypenoubuntu.sh
+# para quem migrou para o Ubuntu 13.10
+# e está com problemas no skype
+
+# solução
+sudo sed -i 's/^Exec=.*/Exec=env PULSE_LATENCY_MSEC=30 skype %U/' /usr/share/applications/skype.desktop
+
+# pede para o usuário verificar solução
+echo "Verifique novamente e veja se agora está tudo ok!"
+
+# fim
