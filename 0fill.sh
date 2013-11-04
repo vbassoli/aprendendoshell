7cc1e26798d3c1a7cafcb843f1c7227d3ea798cd
Gustavo <gustavo@logicus.com.br>
2013-11-04 02:12:11 -0200

    Update 0fill.sh

--- 0fill.sh
+++ 0fill.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+# 0fill.sh
+# limpa totalmente um dispositivo preenchendo-o de zeros
+# útil na computação forense
 
+# limpando a tela
 clear
 
+# dialogando com o usuário
 echo "Digite o dispositivo que deseja zerar (exemplo: /dev/sdc) ou pressione CTRL+C para cancelar:"
 
+# recolhendo o dispositivo pelo usuário
 read dispositivo
+
+# limpando a tela novamente
 clear
+
+# zerando o dispositivo
 echo "Zerando $dispositivo.
 Dependendo do tamanho do seu dispositivo este processo levará um bom tempo.
 Aproveite para tomar um café, ler um artigo, conversar com pessoas.
