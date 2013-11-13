493a627e305ddab66bfd8d8db2eae5836d8783ec
Gustavo <gustavo@logicus.com.br>
2013-11-13 21:18:37 -0200

    Update continua.sh

--- continua.sh
+++ continua.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+# continua.sh
+# script simples que pode ser muito útil
+# se inserido em outros scripts maiores
 
+# recolhe resposta do usuário e encaminha
 read -p "Você deseja continuar (s/n)?"
 [[ ${REPLY^} == N ]] && exit
+
+# em caso de sim
 echo "Continuando..." 
+
+# fim
