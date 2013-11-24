5013f8a39e8467ea25d55b430b5da060f2818272
Gustavo <gustavo@logicus.com.br>
2013-11-24 19:54:47 -0200

    Update instalalibreofficeslack14.sh

--- instalalibreofficeslack14.sh
+++ instalalibreofficeslack14.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # possui um erro no tar
 
-wget http://donate.libreoffice.org/pt/dl/rpm-x86/4.1.3/pt-BR/LibreOffice_4.1.3_Linux_x86_rpm.tar.gz
-tar -xf LibreOffice_4.1.3_Linux_x86_rpm.tar.gz
+wget --progress=dot -c http://donate.libreoffice.org/pt/dl/rpm-x86/4.1.3/pt-BR/LibreOffice_4.1.3_Linux_x86_rpm.tar.gz
+tar -vzxf LibreOffice_4.1.3_Linux_x86_rpm.tar.gz
 cd LibreOffice
 rpm -i *.rpm
