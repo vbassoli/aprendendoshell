289ea4c8953b346b5a96b52397c95723245c2804
Gustavo <gustavo@logicus.com.br>
2013-11-24 21:14:58 -0200

    Update instalalibreofficeslack14.sh

--- instalalibreofficeslack14.sh
+++ instalalibreofficeslack14.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # possui um erro de dependências
 
-wget --progress=dot -c http://download.documentfoundation.org/libreoffice/stable/4.1.3/rpm/x86/LibreOffice_4.1.3_Linux_x86_rpm.tar.gz
-tar -vzxf LibreOffice_4.1.3_Linux_x86_rpm.tar.gz
-cd LibreOffice_4.1.3.2_Linux_x86_rpm/
+wget --progress=dot -c http://download.documentfoundation.org/libreoffice/stable/4.0.6/rpm/x86/LibreOffice_4.0.6_Linux_x86_rpm.tar.gz
+tar -vzxf LibreOffice_4.0.6_Linux_x86_rpm.tar.gz
+cd LibreOffice_4.0.6.2_Linux_x86_rpm/
 cd RPMS/
-rpm -i *.rpm
+rpm -i --nodeps --force -v *.rpm
