dcfc18a9c559a0252e0ec34193ba2fca3f1c925a
Gustavo <gustavo@logicus.com.br>
2013-11-24 14:23:23 -0200

    Update installflashslack14.sh

--- installflashslack14.sh
+++ installflashslack14.sh
@@ -3,7 +3,7 @@
 # instalador do flashplugin no slackware
 # script não testado
 
-wget http://fpdownload.macromedia.com/install_flash_player_11_linux.i386.tar.gz /tmp
+wget http://fpdownload.macromedia.com/get/flashplayer/pdc/11.2.202.238/install_flash_player_11_linux.i386.tar.gz /tmp
 cd /tmp
 tar -xf install_flash_player_11_linux.i386.tar.gz
 cp lib*.so /usr/lib/mozilla/plugins
