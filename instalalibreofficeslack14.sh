29bf7f43fa018a7f23a67b9f41a5a2e4ad4eecdd
Gustavo <gustavo@logicus.com.br>
2013-11-24 19:04:35 -0200

    Update instalalibreofficeslack14.sh

--- instalalibreofficeslack14.sh
+++ instalalibreofficeslack14.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-cd /tmp
-mkdir libre
-cd libre
 wget http://donate.libreoffice.org/pt/dl/rpm-x86/4.1.3/pt-BR/LibreOffice_4.1.3_Linux_x86_rpm.tar.gz
-tar -vzxf LibreOffice_4.1.3_Linux_x86_rpm.tar.gz
+tar -xf LibreOffice_4.1.3_Linux_x86_rpm.tar.gz
 cd LibreOffice
 rpm -i *.rpm
