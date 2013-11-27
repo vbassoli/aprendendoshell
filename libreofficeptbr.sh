e9f2d9483b5c4ae13970029a058a38d9053ebeee
Gustavo <gustavo@logicus.com.br>
2013-11-27 13:03:16 -0200

    Create libreofficeptbr.sh

---
+++ libreofficeptbr.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# instala os pacotes necessários
+# para ter o libreoffice em língua portuguesa
+# para linux mint ou ubuntu
+
+sudo apt-get update
+sudo apt-get install -y libreoffice-l10n-pt-br libreoffice-help-pt-br
+
