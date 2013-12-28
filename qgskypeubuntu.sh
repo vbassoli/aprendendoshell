a35ab068b4929e0edd7a5bd88616fa157f37a801
Gustavo <gustavo@logicus.com.br>
2013-12-27 23:58:29 -0200

    Update and rename arrumaskypenoubuntu.sh to qgskypeubuntu.sh

--- qgskypeubuntu.sh
+++ qgskypeubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# arrumaskypenoubuntu.sh
-# para quem migrou para o Ubuntu 13.10
-# e está com problemas no skype
+# qgskypeubuntu.sh
+# autor: gustavo soares de lima
+# para quem migrou para o Ubuntu 13.10 e está com problemas no skype
 
 # solução
 sudo sed -i 's/^Exec=.*/Exec=env PULSE_LATENCY_MSEC=30 skype %U/' /usr/share/applications/skype.desktop
