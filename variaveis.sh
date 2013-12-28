acc4d0a1621169153def3389583bf9e58e061557
Gustavo <gustavo@logicus.com.br>
2013-12-28 00:52:20 -0200

    Create variaveis.sh

---
+++ variaveis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# variaveis.sh
+# para demonstrar um pouco
+# do tratamento de variáveis
+
+echo "Usuário $LOGNAME. Você está usando o display $DISPLAY. Sua pasta pessoal fica em $HOME. \
+Você saberia me dizer o que contém nos seguintes diretórios: $PATH ?"
+
