1eeefc05d192dddb14299713008040fc471032df
Gustavo <gustavo@logicus.com.br>
2013-12-30 18:39:28 -0200

    Update continua.sh

--- continua.sh
+++ continua.sh
@@ -3,6 +3,9 @@
 # script simples que pode ser muito útil
 # se inserido em outros scripts maiores
 
+# limpando a tela
+clear
+
 # recolhe resposta do usuário e encaminha
 read -p "Você deseja continuar (s/n)?"
 [[ ${REPLY^} == N ]] && exit
