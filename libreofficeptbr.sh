cd2813f312addf4191c17f18b065d66fab756ef1
Gustavo <gustavo@logicus.com.br>
2013-12-30 19:14:06 -0200

    Update libreofficeptbr.sh

--- libreofficeptbr.sh
+++ libreofficeptbr.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# libreofficeptbr.sh
 # instala os pacotes necessários
 # para ter o libreoffice em língua portuguesa
 # para linux mint ou ubuntu
 
+echo "Iniciando instalação em 3 segundos ..."
+sleep 3
 sudo apt-get update
-sudo apt-get install -y libreoffice-l10n-pt-br libreoffice-help-pt-br
 
+# rodando apt para instalação de pacotes
+sudo apt-get install -y libreoffice-l10n-pt-br libreoffice-help-pt-br
+echo "Processo concluído."
