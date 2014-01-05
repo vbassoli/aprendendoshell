4f27521607b8efecb693ae62794ff17a973879e3
Gustavo <gustavo@logicus.com.br>
2014-01-05 14:51:32 -0200

    Update 0fill.sh

--- 0fill.sh
+++ 0fill.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
-# 0fill.sh
-# autor: gustavo soares de lima
-# limpa totalmente um dispositivo preenchendo-o de zeros, útil para segurança de dados
+#
+# 0fill.sh - Realiza um 0fill em um dispositivo indicado
+#
+# Site        : https://github.com/gustavologicus/aprendendoshell/blob/master/0fill.sh
+# Autor       : Gustavo Soares de Lima <gustavo@logicus.com.br>
+# Manutenção  : Gustavo Soares de Lima <gustavo@logicus.com.br>
+#
+# ------------------------------------------------------------------------------------
+# Este programa recebe um dispositivo do usuário via comando read e então realiza um 
+# 0fill no dispositivo indicado. 0fill nada mais é do que o preenchimento de um dispositivo
+# com zeros em todos os seus blocos. Isso é muito útil na área de segurança de dados.
+#
+# Exemplo:
+# $ Digite o dispositivo que deseja zerar (exemplo: /dev/sdc) ou pressione CTRL+C para cancelar:
+# $ /dev/sdc
+# $ Zerando /dev/sdc
+# $ Concluído
+# ------------------------------------------------------------------------------------
+#
+# Histórico:
+#
+# v1.0 2014-01-05, Gustavo Soares de Lima:
+#   - Versão inicial com cabeçalho
+#
+# ------------------------------------------------------------------------------------
+# Licença: GPL.
+#
 
 # limpando a tela
 clear
